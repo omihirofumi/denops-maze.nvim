f9b67068085aa9de998017282c7c3e13b9626f6f
Hirofumi Omi <hirofumi.omi@3-shake.com>
2025-01-10 21:44:20 +0900

    buffer concrete

--- denops/denops-maze/main.ts
+++ denops/denops-maze/main.ts
@@ -1,21 +1,23 @@
 import type { Entrypoint } from "jsr:@denops/std";
+import * as buffer from "jsr:@denops/std/buffer";
 import * as fn from "jsr:@denops/std/function";
 import { Maze } from "npm:@thewizardbear/maze_generator";
 
 export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
     async maze() {
-      await denops.cmd("enew");
+      const { bufnr, winnr } = await buffer.open(denops, "maze://");
 
-      const winWidth = await fn.winwidth(denops, 0);
-      const winHeight = await fn.winheight(denops, 0);
+      const winWidth = await fn.winwidth(denops, winnr);
+      const winHeight = await fn.winheight(denops, winnr);
       const maze = new Maze({
         xSize: winWidth / 3,
         ySize: winHeight / 3,
       }).generate();
       const content = maze.getString();
 
-      await fn.setline(denops, 1, content.split(/\r?\n/g));
+      await buffer.replace(denops, bufnr, content.split(/\r?\n/g));
+      await buffer.concrete(denops, bufnr);
     },
   };
 };
