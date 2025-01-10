286aae8f789ed6e9e9e28c7380031bca8bf63669
Hirofumi Omi <hirofumi.omi@3-shake.com>
2025-01-10 21:36:50 +0900

    add denops functions wrapper

--- denops/denops-maze/main.ts
+++ denops/denops-maze/main.ts
@@ -1,13 +1,21 @@
 import type { Entrypoint } from "jsr:@denops/std";
+import * as fn from "jsr:@denops/std/function";
 import { Maze } from "npm:@thewizardbear/maze_generator";
 
 export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
     async maze() {
-      const maze = new Maze({}).generate();
-      const content = maze.getString();
       await denops.cmd("enew");
-      await denops.call("setline", 1, content.split(/\r?\n/g));
+
+      const winWidth = await fn.winwidth(denops, 0);
+      const winHeight = await fn.winheight(denops, 0);
+      const maze = new Maze({
+        xSize: winWidth / 3,
+        ySize: winHeight / 3,
+      }).generate();
+      const content = maze.getString();
+
+      await fn.setline(denops, 1, content.split(/\r?\n/g));
     },
   };
 };
